d1a2869a6926a988e8b5bce3a4c7d2cc482a10c7
isosatl <isaias_sosa@yahoo.com>
2016-07-07 20:22:13 -0600

    first commit

---
+++ src/test/java/com/two/lights/DemoStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.two.lights;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoStarterApplication.class)
+public class DemoStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
